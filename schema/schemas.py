5cc94f06dbfabb55f10750d100d1936b2a9181d5
tperezz <tperezzorraquin@gmail.com>
2025-10-06 11:49:08 -0300

    Add Jira users entity to ETL

--- schema/schemas.py
+++ schema/schemas.py
@@ -39,3 +39,22 @@ class Esquema(object):
         bigquery.SchemaField("epic_key",                   "STRING"),
         bigquery.SchemaField("epic_name",                  "STRING"),
     ]
+
+    schema_users = [
+        bigquery.SchemaField("account_id",          "STRING"),
+        bigquery.SchemaField("account_type",        "STRING"),
+        bigquery.SchemaField("account_status",      "STRING"),
+        bigquery.SchemaField("display_name",        "STRING"),
+        bigquery.SchemaField("public_name",         "STRING"),
+        bigquery.SchemaField("email_address",       "STRING"),
+        bigquery.SchemaField("active",              "BOOL"),
+        bigquery.SchemaField("time_zone",           "STRING"),
+        bigquery.SchemaField("locale",              "STRING"),
+        bigquery.SchemaField("self_url",            "STRING"),
+        bigquery.SchemaField("avatar_16x16",        "STRING"),
+        bigquery.SchemaField("avatar_24x24",        "STRING"),
+        bigquery.SchemaField("avatar_32x32",        "STRING"),
+        bigquery.SchemaField("avatar_48x48",        "STRING"),
+        bigquery.SchemaField("groups",              "STRING"),
+        bigquery.SchemaField("application_roles",   "STRING"),
+    ]
